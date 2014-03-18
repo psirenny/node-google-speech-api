226ccd79174b878cddbcd8093aaa6a3e664e2d0d
psirenny <djtorres0@gmail.com>
2014-03-18 12:57:31 -0400

    cleanup tests

--- test/index.js
+++ test/index.js
@@ -2,6 +2,19 @@ var path = require('path')
   , should = require('chai').should()
   , speech = require('../index');
 
+function check(text, done) {
+  return function (err, res) {
+    if (err) throw err;
+    res.should.be.an('array');
+    res[0].should.be.an('object');
+    res[0].status.should.equal(0);
+    res[0].hypotheses.should.be.an('array');
+    res[0].hypotheses[0].should.be.an('object');
+    res[0].hypotheses[0].utterance.should.equal(text);
+    done();
+  };
+}
+
 describe('speech', function () {
   var en = path.join(__dirname, 'fixtures/en.mp3')
     , es = path.join(__dirname, 'fixtures/es.mp3')
@@ -13,71 +26,30 @@ describe('speech', function () {
 
   it('should take a string', function (done) {
     this.timeout(2000);
-    speech(en, function (err, res) {
-      if (err) throw err;
-      res.should.be.an('array');
-      res[0].should.be.an('object');
-      res[0].status.should.equal(0);
-      res[0].hypotheses.should.be.an('array');
-      res[0].hypotheses[0].should.be.an('object');
-      res[0].hypotheses[0].utterance.should.equal('thank you very much');
-      done();
-    });
+    speech(en, check('thank you very much', done));
   });
 
   it('should take an object', function (done) {
     this.timeout(2000);
-    speech({file: en}, function (err, res) {
-      if (err) throw err;
-      res.should.be.an('array');
-      res[0].should.be.an('object');
-      res[0].status.should.equal(0);
-      res[0].hypotheses.should.be.an('array');
-      res[0].hypotheses[0].should.be.an('object');
-      res[0].hypotheses[0].utterance.should.equal('thank you very much');
-      done();
-    });
+    var opts = {file: en};
+    speech(opts, check('thank you very much', done));
   });
 
   it('should work in another language', function (done) {
     this.timeout(2000);
-    speech({file: es, lang: 'es'}, function (err, res) {
-      if (err) throw err;
-      res.should.be.an('array');
-      res[0].should.be.an('object');
-      res[0].status.should.equal(0);
-      res[0].hypotheses.should.be.an('array');
-      res[0].hypotheses[0].should.be.an('object');
-      res[0].hypotheses[0].utterance.should.equal('muchas gracias');
-      done();
-    });
+    var opts = {file: es, lang: 'es'};
+    speech(opts, check('muchas gracias', done));
   });
 
   it('should censor profanity', function (done) {
     this.timeout(3000);
-    speech({file: profanity, pfilter: true}, function (err, res) {
-      if (err) throw err;
-      res.should.be.an('array');
-      res[0].should.be.an('object');
-      res[0].status.should.equal(0);
-      res[0].hypotheses.should.be.an('array');
-      res[0].hypotheses[0].should.be.an('object');
-      res[0].hypotheses[0].utterance.should.equal('the f*** out my face with that s***');
-      done();
-    });
+    var opts = {file: profanity, pfilter: true};
+    speech(opts, check('the f*** out my face with that s***', done));
   });
 
   it('should not censor profanity', function (done) {
     this.timeout(3000);
-    speech({file: profanity, pfilter: false}, function (err, res) {
-      if (err) throw err;
-      res.should.be.an('array');
-      res[0].should.be.an('object');
-      res[0].status.should.equal(0);
-      res[0].hypotheses.should.be.an('array');
-      res[0].hypotheses[0].should.be.an('object');
-      res[0].hypotheses[0].utterance.should.equal('the fuck out my face with that shit');
-      done();
-    });
+    var opts = {file: profanity, pfilter: false};
+    speech(opts, check('the fuck out my face with that shit', done));
   });
 });
