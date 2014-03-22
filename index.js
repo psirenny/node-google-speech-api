ade8e293bf4422d5c06fb2aab6a1ba45ef508a1f
psirenny <djtorres0@gmail.com>
2014-03-22 12:28:50 -0400

    return a stream

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var _ = require('lodash')
   , fs = require('fs')
   , format = require('google-speech-format')
   , qs = require('qs')
-  , request = require('request');
+  , request = require('request')
+  , through = require('through');
 
 module.exports = function (options, callback) {
   var params = {
@@ -28,9 +29,10 @@ module.exports = function (options, callback) {
   });
 
   var headers = {'content-type': 'audio/x-flac; rate=' + options.sampleRate}
+    , stream = through().pause()
     , url = 'https://www.google.com/speech-api/v1/recognize?' + qs.stringify(params);
 
-  var getSpeech = function (file, callback) {
+  function getSpeech(file, callback) {
     fs.readFile(file, function (err, data) {
       if (err) return callback(err);
 
@@ -53,8 +55,15 @@ module.exports = function (options, callback) {
     });
   };
 
+  function onEnd(err, res) {
+    callback(err, res);
+    stream.resume().queue(JSON.stringify(res)).end();
+  }
+
   format(options, function (err, files) {
     if (err) return callback(err);
-    async.mapLimit(files, options.maxRequests, getSpeech, callback);
+    async.mapLimit(files, options.maxRequests, getSpeech, onEnd);
   });
+
+  return stream;
 };
--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 var _ = require('lodash')
+  , concat = require('concat-stream')
   , natural = require('natural')
   , path = require('path')
   , should = require('chai').should()
@@ -13,19 +14,22 @@ function combine(utterance, res) {
   return utterance + space + res.hypotheses[0].utterance;
 }
 
-function check(text, done) {
-  return function (err, res) {
+function check(opts, text, done) {
+  var stream = speech(opts, function (err, res) {
     if (err) throw err;
-    res.should.be.an('array');
-    res[0].should.be.an('object');
-    res[0].status.should.equal(0);
-    res[0].hypotheses.should.be.an('array');
-    res[0].hypotheses[0].should.be.an('object');
-    var sentence = _.reduce(res, combine, '');
-    var distance = natural.JaroWinklerDistance(sentence, text);
-    distance.should.be.at.least(.8);
-    done();
-  };
+    stream.pipe(concat(function (data) {
+      res.should.deep.equal(JSON.parse(data));
+      res.should.be.an('array');
+      res[0].should.be.an('object');
+      res[0].status.should.equal(0);
+      res[0].hypotheses.should.be.an('array');
+      res[0].hypotheses[0].should.be.an('object');
+      var sentence = _.reduce(res, combine, '');
+      var distance = natural.JaroWinklerDistance(sentence, text);
+      distance.should.be.at.least(.8);
+      done();
+    }));
+  });
 }
 
 describe('speech', function () {
@@ -35,36 +39,36 @@ describe('speech', function () {
 
   it('should take a string', function (done) {
     this.timeout(2000);
-    speech(en, check('thank you very much', done));
+    check(en, 'thank you very much', done);
   });
 
   it('should take an object', function (done) {
     this.timeout(2000);
     var opts = {file: en};
-    speech(opts, check('thank you very much', done));
+    check(en, 'thank you very much', done);
   });
 
   it('should work in another language', function (done) {
     this.timeout(2000);
     var opts = {file: es, lang: 'es'};
-    speech(opts, check('muchas gracias', done));
+    check(opts, 'muchas gracias', done);
   });
 
   it('should censor profanity', function (done) {
     this.timeout(3000);
     var opts = {file: profanity, pfilter: true};
-    speech(opts, check('f*** you', done));
+    check(opts, 'f*** you', done);
   });
 
   it('should not censor profanity', function (done) {
     this.timeout(3000);
     var opts = {file: profanity, pfilter: false};
-    speech(opts, check('fuck you', done));
+    check(opts, 'fuck you', done);
   });
 
   it('should clip long audio', function (done) {
     this.timeout(12000);
     var opts = {file: lengthy};
-    speech(opts, check('1 of the Iliad of Homer rendered into English flag vs spy Edward Earl of Derby this is a liberal Vox recording recordings are in the public domain for more information or to volunteer please visit fox.org', done));
+    check(opts, '1 of the Iliad of Homer rendered into English flag vs spy Edward Earl of Derby this is a liberal Vox recording recordings are in the public domain for more information or to volunteer please visit fox.org', done);
   });
 });
